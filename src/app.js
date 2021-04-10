85e05cdd3141b0d6f1d4a0516b0f3221cab0d8b4
glorious73 <amjadmh73@gmail.com>
2021-04-10 20:41:23 +0300

    updated packages and fixed imports

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-const express    = require("express");
-const bodyParser = require("body-parser");
-const morgan     = require("morgan");
-const port       = process.env.PORT || 5000;
-const home       = require("./routes/home");
-const path       = require('path');
-const app        = express();
+import express from "express";
+import morgan from "morgan";
+import path from "path";
+import home from "./routes/home.js"; // not all resolvers will find files without ".js"
+
+const app   = express(); 
+const port  = process.env.PORT || 5000;
+const __dirname = (() => {let x = path.dirname(decodeURI(new URL(import.meta.url).pathname)); return path.resolve( (process.platform == "win32") ? x.substr(1) : x ); })();
 
 //the limit is a safty feature, that we can ommit later
 app.use(express.urlencoded({ limit: '2mb', extended: true }));
